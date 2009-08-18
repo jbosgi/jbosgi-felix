c45d676236f90df70b20f46004577cfde6c619f6
thomas.diesler@jboss.com <thomas.diesler@jboss.com@84be2c1e-ba19-0410-b317-a758671a6fc1>
2009-08-18 08:51:46 +0000

    Add EqunioxIntegration
    git-svn-id: https://anonsvn.jboss.org/repos/jbossas/projects/jboss-osgi/projects/runtime/felix/trunk@92501 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/main/java/org/jboss/osgi/felix/FelixIntegration.java
+++ src/main/java/org/jboss/osgi/felix/FelixIntegration.java
@@ -23,80 +23,23 @@ package org.jboss.osgi.felix;
 
 //$Id$
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.jboss.logging.Logger;
-import org.jboss.osgi.spi.FrameworkException;
-import org.jboss.osgi.spi.logging.ExportedPackageHelper;
+import org.jboss.osgi.spi.framework.FrameworkIntegration;
 import org.jboss.osgi.spi.util.ServiceLoader;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
-import org.osgi.framework.launch.Framework;
 import org.osgi.framework.launch.FrameworkFactory;
 
 /**
- * An abstraction of an OSGi Framework
+ * Felix specific OSGi Framework integration.
  * 
  * @author thomas.diesler@jboss.com
  * @since 23-Jan-2009
  */
-public class FelixIntegration
+public class FelixIntegration extends FrameworkIntegration
 {
    // Provide logging
    final Logger log = Logger.getLogger(FelixIntegration.class);
-
-   private Map<String, Object> properties = new HashMap<String, Object>();
-   private List<URL> autoInstall = new ArrayList<URL>();
-   private List<URL> autoStart = new ArrayList<URL>();
-
-   private Framework framework;
-
-   public Map<String, Object> getProperties()
-   {
-      return properties;
-   }
-
-   public void setProperties(Map<String, Object> props)
-   {
-      this.properties = props;
-   }
-
-   public List<URL> getAutoInstall()
-   {
-      return autoInstall;
-   }
-
-   public void setAutoInstall(List<URL> autoInstall)
-   {
-      this.autoInstall = autoInstall;
-   }
-
-   public List<URL> getAutoStart()
-   {
-      return autoStart;
-   }
-
-   public void setAutoStart(List<URL> autoStart)
-   {
-      this.autoStart = autoStart;
-   }
-
-   public Bundle getBundle()
-   {
-      assertFrameworkStart();
-      return framework;
-   }
-
-   public BundleContext getBundleContext()
-   {
-      return getBundle().getBundleContext();
-   }
-
+   
    public void create()
    {
       String implVersion = getClass().getPackage().getImplementationVersion();
@@ -112,76 +55,6 @@ public class FelixIntegration
       // Load the framework instance
       FrameworkFactory factory = ServiceLoader.loadService(FrameworkFactory.class);
       framework = factory.newFramework(properties);
-   }
-
-   public void start()
-   {
-      // Create the Felix instance
-      assertFrameworkCreate();
-      
-      // Start the System Bundle
-      try
-      {
-         framework.start();
-      }
-      catch (BundleException ex)
-      {
-         throw new FrameworkException("Cannot start system bundle", ex);
-      }
-      
-      // Get system bundle context
-      BundleContext context = framework.getBundleContext();
-      if (context == null)
-         throw new FrameworkException("Cannot obtain system context");
-
-      // Log the the framework packages
-      ExportedPackageHelper packageHelper = new ExportedPackageHelper(context);
-      packageHelper.logExportedPackages(getBundle());
-      
-      Map<URL, Bundle> autoBundles = new HashMap<URL, Bundle>();
-
-      // Add the autoStart bundles to autoInstall
-      for (URL bundleURL : autoStart)
-      {
-         autoInstall.add(bundleURL);
-      }
-
-      // Install autoInstall bundles
-      for (URL bundleURL : autoInstall)
-      {
-         try
-         {
-            Bundle bundle = context.installBundle(bundleURL.toString());
-            long bundleId = bundle.getBundleId();
-            log.info("Installed bundle [" + bundleId + "]: " + bundle.getSymbolicName());
-            autoBundles.put(bundleURL, bundle);
-         }
-         catch (BundleException ex)
-         {
-            stop();
-            throw new IllegalStateException("Cannot install bundle: " + bundleURL, ex);
-         }
-      }
-
-      // Start autoStart bundles
-      for (URL bundleURL : autoStart)
-      {
-         try
-         {
-            Bundle bundle = autoBundles.get(bundleURL);
-            if (bundle != null)
-            {
-               bundle.start();
-               packageHelper.logExportedPackages(bundle);
-               log.info("Started bundle: " + bundle.getSymbolicName());
-            }
-         }
-         catch (BundleException ex)
-         {
-            stop();
-            throw new IllegalStateException("Cannot start bundle: " + bundleURL, ex);
-         }
-      }
    }
 
    public void stop()
@@ -236,18 +109,5 @@ public class FelixIntegration
          if (timeout == 0 && thread.isAlive())
             thread.interrupt();
       }
-   }
-
-   private void assertFrameworkCreate()
-   {
-      if (framework == null)
-         create();
-   }
-
-   private void assertFrameworkStart()
-   {
-      assertFrameworkCreate();
-      if ((framework.getState() & Bundle.ACTIVE) == 0)
-         start();
    }
 }
