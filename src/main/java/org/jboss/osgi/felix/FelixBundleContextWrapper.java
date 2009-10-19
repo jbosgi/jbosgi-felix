9e8939d0cb97c15ff61fba2092cf3ec5685616ff
thomas.diesler@jboss.com <thomas.diesler@jboss.com@84be2c1e-ba19-0410-b317-a758671a6fc1>
2009-10-19 08:13:35 +0000

    Seperate notion of deployer from interceptor
    git-svn-id: https://anonsvn.jboss.org/repos/jbossas/projects/jboss-osgi/projects/runtime/felix/trunk@95096 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/main/java/org/jboss/osgi/felix/FelixBundleContextWrapper.java
+++ src/main/java/org/jboss/osgi/felix/FelixBundleContextWrapper.java
@@ -26,7 +26,7 @@ package org.jboss.osgi.felix;
 import java.net.URL;
 
 import org.jboss.logging.Logger;
-import org.jboss.osgi.deployment.DeployerService;
+import org.jboss.osgi.deployment.deployer.DeployerService;
 import org.jboss.osgi.spi.framework.BundleContextWrapper;
 import org.jboss.osgi.spi.util.BundleInfo;
 import org.osgi.framework.Bundle;
